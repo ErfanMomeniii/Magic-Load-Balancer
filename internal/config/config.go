f69fcd3262014b5b08661d7cf9e91c75e3184755
Erfan Momeni <Erfan.Momeni@snapp.cab>
2022-10-21 11:42:32 +0330

    add config init method

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,8 +1,17 @@
 package config
 
-import "time"
+import (
+	"fmt"
+	"github.com/mitchellh/mapstructure"
+	"github.com/spf13/viper"
+	"strings"
+	"time"
+)
 
-var C *Config
+var (
+	Default = "config.yaml"
+	C       *Config
+)
 
 type Config struct {
 	Endpoints []Endpoint `yaml:"endpoints"`
@@ -48,4 +57,33 @@ type HTTPServer struct {
 	WriteTimeout      time.Duration `yaml:"write_timeout"`
 	ReadHeaderTimeout time.Duration `yaml:"read_header_timeout"`
 	IdleTimeout       time.Duration `yaml:"idle_timeout"`
+}
+
+func Init(filename string) *Config {
+	c := new(Config)
+	v := viper.New()
+	v.SetConfigType("yaml")
+	v.AddConfigPath(".")
+	v.SetEnvKeyReplacer(strings.NewReplacer(".", "_", "-", "_"))
+	v.AutomaticEnv()
+	v.SetConfigName(Default)
+
+	if err := v.ReadInConfig(); err != nil {
+		panic(fmt.Errorf("error loading configs: %s", err.Error()).(any))
+	}
+
+	err := v.Unmarshal(c, func(config *mapstructure.DecoderConfig) {
+		config.TagName = "yaml"
+		config.DecodeHook = mapstructure.ComposeDecodeHookFunc(
+			mapstructure.StringToTimeDurationHookFunc(),
+			mapstructure.StringToSliceHookFunc(","),
+		)
+	})
+	if err != nil {
+		panic(fmt.Errorf("failed on config `%s` unmarshal: %w", Default, err).(any))
+	}
+
+	C = c
+
+	return c
 }
