2618346a7beea1e65aae9d7bf77830086d81211e
Erfan Momeni <Erfan.Momeni@snapp.cab>
2022-10-30 17:04:44 +0330

    fix some design pattern problem

--- internal/db/redis.go
+++ internal/db/redis.go
@@ -3,9 +3,7 @@ package db
 import (
 	"context"
 	"github.com/ErfanMomeniii/Magic-Load-Balancer/internal/config"
-	"github.com/ErfanMomeniii/Magic-Load-Balancer/internal/log"
 	"github.com/go-redis/redis/v8"
-	"math"
 	"time"
 )
 
@@ -42,39 +40,4 @@ func (rc *RedisConnection) Get(key string) (interface{}, error) {
 	val, err := rc.Client.Get(context.Background(), key).Result()
 
 	return val, err
-}
-
-func (rc *RedisConnection) ChooseServerRoundly(service config.Service) config.Server {
-	index, err := rc.Get(service.Name)
-
-	if err == redis.Nil {
-		index = 0
-	}
-
-	err = rc.Set(service.Name, (index.(int)+1)%len(service.Servers))
-	if err != nil {
-		log.Logger.Error(err.Error())
-	}
-
-	return service.Servers[index.(int)]
-}
-
-func (rc *RedisConnection) ChooseServerMagically(service config.Service) config.Server {
-	index := 0
-	minTime := math.MaxInt
-	for i, server := range service.Servers {
-		serverWorkingTime, err := rc.Get(server.IP)
-
-		if err == redis.Nil {
-			serverWorkingTime = 0
-			_ = rc.Set(server.IP, 0)
-		}
-
-		if serverWorkingTime.(int) < minTime {
-			minTime = serverWorkingTime.(int)
-			index = i
-		}
-	}
-
-	return service.Servers[index]
 }
---
+++ internal/repository/server_selection.go
@@ -0,0 +1,53 @@
+package repository
+
+import (
+	"github.com/ErfanMomeniii/Magic-Load-Balancer/internal/config"
+	"github.com/ErfanMomeniii/Magic-Load-Balancer/internal/db"
+	"github.com/ErfanMomeniii/Magic-Load-Balancer/internal/log"
+	"github.com/go-redis/redis/v8"
+	"math"
+)
+
+type ServerSelection interface {
+	SelectServerRoundly(service config.Service) config.Server
+	SelectServerMagically(service config.Service) config.Server
+}
+
+type ChooseServerHandler struct {
+	DB db.DB
+}
+
+func (csh *ChooseServerHandler) SelectServerRoundly(service config.Service) config.Server {
+	index, err := csh.DB.Get(service.Name)
+
+	if err == redis.Nil {
+		index = 0
+	}
+
+	err = csh.DB.Set(service.Name, (index.(int)+1)%len(service.Servers))
+	if err != nil {
+		log.Logger.Error(err.Error())
+	}
+
+	return service.Servers[index.(int)]
+}
+
+func (csh *ChooseServerHandler) SelectServerMagically(service config.Service) config.Server {
+	index := 0
+	minTime := math.MaxInt
+	for i, server := range service.Servers {
+		serverWorkingTime, err := csh.DB.Get(server.IP)
+
+		if err == redis.Nil {
+			serverWorkingTime = 0
+			_ = csh.DB.Set(server.IP, 0)
+		}
+
+		if serverWorkingTime.(int) < minTime {
+			minTime = serverWorkingTime.(int)
+			index = i
+		}
+	}
+
+	return service.Servers[index]
+}
